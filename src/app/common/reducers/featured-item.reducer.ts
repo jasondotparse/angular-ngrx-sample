76931d41ce6103ed750345315b4d601517b5f296
Holtkamp, Jason <jholtkamp@conversantmedia.com>
2018-10-12 15:44:10 -0500

    removed default task

--- src/app/common/reducers/featured-item.reducer.ts
+++ src/app/common/reducers/featured-item.reducer.ts
@@ -1,7 +1,7 @@
 import { Map } from 'immutable';
 import { TodoItem } from './../models/appState.model';
 
-const defaultState = Map({task: 'hi', comment: 'test'});
+const defaultState = Map({task: '', comment: ''});
 
 export function FeaturedItemReducer(state = defaultState,  action): Map<string, string> {
   switch (action.type) {
